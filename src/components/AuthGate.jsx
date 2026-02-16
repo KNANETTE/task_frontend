3362ba6c4b4f0b5c9a3397072478afb59601ef86
Nana KOITA <koita.nana19@gmail.com>
2026-02-16 04:57:23 +0100

    mise en place d'un filtre pour gérer la redirection vers l'authtification

---
+++ src/components/AuthGate.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { isLogged } from "../services/authServices";
+
+export default function AuthGate({ children }) {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+
+    async function checkAuth() {
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+            navigate("/auth");
+            setLoading(false);
+            return;
+        }
+
+        const res = await isLogged(token)
+
+        if (!res.ok) {
+            localStorage.removeItem("token");
+            navigate("/auth");
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        checkAuth();
+    }, []);
+
+    if (loading)
+        return <div>Loading...</div>;
+    return children;
+}
