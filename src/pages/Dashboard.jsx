7399e09291ee92ab2b271d91290ba299a81feeda
Nana KOITA <koita.nana19@gmail.com>
2026-03-03 06:27:36 +0100

    mise a jour des components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,30 @@
-import Navbar from "../components/Navbar"
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Toolbar from '@mui/material/Toolbar';
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Spaces from "../components/Spaces";
+import WorkspaceModal from '../components/WorkspaceModal';
 
 export default function Dashboard() {
+    const username = localStorage.getItem("username")
+
     return (
         <>
-            <Navbar></Navbar>
-            {/* <Workspaces /> */}
-            {/* <Boards /> */}
-            {/* <Footer /> */}
+            <Toolbar sx={{ margin: "0.5rem" }} />
+            <Navbar />
+            <div>
+                <Typography variant="h4" color="primary">Bienvenue {username}</Typography>
+                <WorkspaceModal />
+            </div>
+            <Divider sx={{ margin: "1rem" }} />
+            <Typography variant="h5" align="center" color="secondary">VOS ESPACES 🌌</Typography>
+            <Spaces />
+            <Toolbar sx={{ margin: "0.5rem" }} />
+            <Footer />
         </>
     )
 }
