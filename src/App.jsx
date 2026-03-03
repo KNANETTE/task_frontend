e09de256ee58fc71753a0e7f2582d0e614d72f42
Nana KOITA <koita.nana19@gmail.com>
2026-03-03 04:45:14 +0100

    mise à jour des liens

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Route, Routes } from "react-router"
 import AuthGate from "./components/AuthGate"
+import About from "./pages/About"
 import Auth from "./pages/Auth"
+import Board from "./pages/Board"
 import Dashboard from "./pages/Dashboard"
+import Profile from "./pages/Profile"
+import Workspace from "./pages/Workspace"
+
 
 function App() {
   return (
@@ -9,6 +14,10 @@ function App() {
       <Routes>
         <Route index element={<Dashboard />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/boards/:id" element={<Board />} />
+        <Route path="/workspaces/:id" element={<Workspace />} />
       </Routes>
     </AuthGate>
   )
