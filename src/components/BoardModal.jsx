53d0e0246bafa0139e6c0e6eef65a954c7ffdf41
Nana KOITA <koita.nana19@gmail.com>
2026-03-05 18:36:21 +0100

    mise à jour

--- src/components/BoardModal.jsx
+++ src/components/BoardModal.jsx
@@ -1,36 +1,12 @@
-// import { useState } from 'react';
-// import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import Modal from '@mui/material/Modal';
-// import AddIcon from '@mui/icons-material/Add';
-// import TextField from '@mui/material/TextField';
-// import FormGroup from '@mui/material/FormGroup';
 import { createBoard } from '../services/boardServices';
-import { useNavigate, useParams } from 'react-router';
-import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Typography from '@mui/material/Typography';
 import Modal from 'react-bootstrap/Modal';
 import AddIcon from '@mui/icons-material/Add';
-import TextField from '@mui/material/TextField';
-import FormGroup from '@mui/material/FormGroup';
-import { createWorkspace } from '../services/workspaceServices';
-// import { useNavigate } from 'react-router';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
 export default function BoardModal({ onResult, onCreated }) {
     const { id } = useParams()
@@ -43,7 +19,6 @@ export default function BoardModal({ onResult, onCreated }) {
     const handleOpen = () => setOpen(!open);
 
     async function handleSubmit() {
-        // console.log("ok")
         try {
             const boardData = JSON.stringify({
                 data: {
@@ -106,26 +81,6 @@ export default function BoardModal({ onResult, onCreated }) {
                     <Button variant='primary' onClick={handleSubmit}>Enregistrer</Button>
                 </Modal.Footer>
             </Modal>
-            {/* <Button variant="contained" color='warning' startIcon={<AddIcon />} onClick={handleOpen}>
-                <Typography noWrap component="div" sx={{ display: { xs: 'none', sm: 'block' } }}>
-                    Créer un tableau
-                </Typography>
-            </Button>
-            <Modal open={open} onClose={handleOpen} aria-labelledby="modal-new-board">
-                <Box sx={style}>
-                    <Typography id="modal-new-board" variant="h6" component="h2">
-                        Un nouveau tableau ?
-                    </Typography>
-                    <form onSubmit={handleSubmit}>
-                        <FormGroup>
-                            <TextField id="title" value={title} label="Titre" variant="standard" name="title" onChange={(e) => setTitle(e.target.value)} />
-                            <label htmlFor="bg-color">Arrière plan</label>
-                            <input type="color" value={background} name="background" id="background" onChange={(e) => setBackground(e.target.value)} />
-                            <Button type='submit'>CREER</Button>
-                        </FormGroup>
-                    </form>
-                </Box>
-            </Modal> */}
         </>
     );
 }
--- src/components/Spaces.jsx
+++ src/components/Spaces.jsx
@@ -15,7 +15,7 @@ export default function Spaces({ content, onDelete, onResult, workspace = null }
                                 url={url}
                                 onDelete={onDelete}
                                 onResult={onResult}
-                                workspace={false}
+                                workspace={!workspace ? true : false}
                             />
                         </Box>
                     )
@@ -25,67 +25,3 @@ export default function Spaces({ content, onDelete, onResult, workspace = null }
     )
     return children
 }
-
-// import Box from "@mui/material/Box";
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Typography from "@mui/material/Typography";
-// import { useEffect, useState } from "react";
-// import { getBoards } from '../services/boardServices';
-// import { getWorkspaces } from "../services/workspaceServices";
-// import Space from "./Space";
-
-
-// export default function Spaces({ workspaceID = null }) {
-//     const token = localStorage.getItem("token")
-//     const url = !workspaceID ? '/workspaces' : '/boards'
-//     const [content, setContent] = useState([])
-//     const [loading, setLoading] = useState(true)
-//     const [error, setError] = useState(null)
-
-//     const children = (
-//         <>
-//             <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center" }}>
-//                 {content.map((data) => {
-//                     return (
-//                         <Box key={data.id} sx={{ display: "flex", padding: "2rem" }}>
-//                             <Space title={data.title} background={data.background} url={`${url}/${data.documentId}`} />
-//                         </Box>
-//                     )
-//                 })}
-//             </Box>
-//         </>
-//     )
-
-//     async function loadContent(workspaceID = null) {
-//         setError(null)
-//         try {
-//             const response = !workspaceID ?
-//                 await getWorkspaces(token) :
-//                 await getBoards(token)
-//             const data = await response.json()
-
-//             if (!response.ok) {
-//                 setError(data.error?.message || "Une erreur s'est produite, veuillez reessayer plus tard")
-//                 setLoading(false)
-//                 return
-//             }
-//             const toLoad = !workspaceID ?
-//                 data.workspaces.reverse() :
-//                 data.data.reverse()
-//             setContent(toLoad)
-//             setLoading(false)
-//         } catch (err) {
-//             setError("Impossible de joindre le serveur!")
-//             console.error(err)
-//             setLoading(false)
-//         }
-//     }
-
-//     useEffect(() => {
-//         loadContent(workspaceID)
-//     }, [workspaceID])
-
-//     if (loading) return <Typography variant="h1"><CircularProgress /></Typography>
-//     if (error) return <Typography variant="h4">{error}</Typography>
-//     return children
-// }
