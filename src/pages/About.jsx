33f9dce06bab618dd062a3da0a03b8129cb8b9eb
Nana KOITA <koita.nana19@gmail.com>
2026-03-11 12:40:05 +0100

    Finalisation de la page A propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
 import Toolbar from "@mui/material/Toolbar"
 import Navbar from "../components/Navbar"
+import { Engineering } from "@mui/icons-material"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 
 export default function About() {
     return (
         <>
             <Navbar />
             <Toolbar sx={{ margin: "0.5rem" }} />
+            <Box sx={{color:"#ddd", fontSize:250}}>
+                <Engineering fontSize="larger"/>
+                <Typography variant="h5">Nous arrivons bientôt!</Typography>
+            </Box>
         </>
     )
 }
