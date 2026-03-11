79c79b2099ef916f6b4ea234c697b101912c6f51
Nana KOITA <koita.nana19@gmail.com>
2026-03-11 22:04:22 +0100

    Finalisation du drag n drop

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -1,51 +1,104 @@
+import { SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Delete, Edit } from "@mui/icons-material";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import { useParams } from "react-router";
-import { deleteList } from "../services/listServices";
+import FormControl from "react-bootstrap/esm/FormControl";
+import Form from "react-bootstrap/Form";
+import { deleteList, updateList } from "../services/listServices";
+import CreateCard from "./CreateCard";
+import ListCard from "./ListCard";
 import ListCards from "./ListCards";
-import { useSortable } from "@dnd-kit/sortable"
-import { CSS } from "@dnd-kit/utilities";
 
-export default function BoardList({ content, onDeleted, onResult }) {
+export default function BoardList({ list, cards, onRequest, onResult }) {
     const token = localStorage.getItem("token")
-    const { bid } = useParams()
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: content.order })
+    const [title, setTitle] = useState(list.title)
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: `list-${list.order}` })
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
+        opacity: isDragging ? 0.5 : 1,
         minWidth: 340,
         background: "#eee",
         height: "fit-content",
     }
     const handleDelete = async () => {
         try {
-            const resp = await deleteList(token, content.documentId)
+            const resp = await deleteList(token, list.documentId)
             if (!resp.ok) {
                 onResult({ success: false, message: "Erreur client/server" })
                 console.error(resp)
                 return
             }
-            onDeleted(bid)
+            onRequest()
             onResult({ success: true, message: "Liste supprimée!" })
         } catch (error) {
             onResult({ success: false, message: "Problème de connexion" })
             console.error(e)
         }
     }
-    const handleUpdate = async()=>{}
+    const handleUpdate = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const data = JSON.stringify({ data: { title } })
+        try {
+            const resp = await updateList(token, list.documentId, data)
+            if (!resp.ok) {
+                console.error(resp)
+                onResult({ success: false, message: "Erreur" })
+                return
+            }
+            onRequest()
+            onResult({ success: true, message: "Modification enregistrée" })
+            setOpen(!open)
+        } catch (error) {
+            onResult({ success: false, message: "Problème de connexion" })
+            console.error(error)
+        }
+        setLoading(false)
+    }
+    const titleField = (
+        <Form onSubmit={handleUpdate} className="d-flex flex-column gap-2">
+            <FormControl type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+            <Box className="d-flex justify-content-evenly">
+                <Button variant="secondary" onClick={() => setOpen(!open)}>Annuler</Button>
+                <Button disabled={loading} type="submit">{loading ? <CircularProgress size={20} color="#000" /> : "Enregistrer"}</Button>
+            </Box>
+        </Form>
+    )
+
     return (
-        <Card ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <CardContent sx={{ fontWeight: "bold", fontSize: "large" }} className="d-flex justify-content-between">
-                {content.title}
-                <Box className="d-flex justify-content-evenly align-items-start" sx={{ width: "30%" }}>
-                    <Button className="text-secondary" style={{ background: "#0000", border: 0 }}><Edit /></Button>
-                    <Button className="text-danger" onClick={handleDelete} style={{ background: "#0000", border: 0 }}><Delete /></Button>
-                </Box>
-            </CardContent>
-            <ListCards id={content.documentId} onResult={onResult} />
+        <Card ref={setNodeRef} style={style} {...attributes}>
+            {!open ? (
+                <CardContent sx={{ background: list.background, fontSize: "large" }} className="d-flex justify-content-between">
+                    <Box {...listeners} sx={{ width: "75%", display: "flex", alignItems: "center" }}>
+                        <Typography variant="h6">{list.title}</Typography>
+                    </Box>
+                    <Box className="d-flex justify-content-evenly align-items-start">
+                        <Button className="text-secondary" onClick={() => setOpen(!open)} style={{ background: "#0000", border: 0 }}><Edit /></Button>
+                        <Button className="text-danger" onClick={handleDelete} style={{ background: "#0000", border: 0 }}><Delete /></Button>
+                    </Box>
+                </CardContent>
+            ) : (
+                <CardContent>
+                    {titleField}
+                </CardContent>
+            )}
+            <ListCards listOrder={list.order} >
+                <CardContent>
+                    <SortableContext items={cards.map(card => `${card.id}`)} strategy={verticalListSortingStrategy}>
+                        {cards.map(card => <ListCard card={card} onDeleted={onRequest} onResult={onResult} />)}
+                    </SortableContext>
+                    <CreateCard id={list.documentId} order={cards.length} onResult={onResult} onCreated={onRequest} />
+                </CardContent>
+            </ListCards>
         </Card>
     )
 }
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,10 +1,22 @@
+import { defaultAnimateLayoutChanges, useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Delete } from "@mui/icons-material";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 import { deleteCard } from "../services/cardServices";
 
-export default function ListCard({ content, onDeleted, onResult }) {
+export default function ListCard({ card, onDeleted, onResult }) {
     const token = localStorage.getItem("token")
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+        id: `${card.id}`,
+        animateLayoutChanges: (args) => defaultAnimateLayoutChanges({ ...args, wasDragging: true })
+    })
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.4 : 1,
+    }
     const handleDelete = async () => {
         try {
             const resp = await deleteCard(token, content.documentId)
@@ -22,8 +34,13 @@ export default function ListCard({ content, onDeleted, onResult }) {
         }
     }
     return (
-        <ListGroupItem className="d-flex justify-content-between align-items-center truncate-3">
-            {content.content}
+        <ListGroupItem
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            className="d-flex justify-content-between align-items-center"
+        >
+            <Box {...listeners} sx={{ width: "80%" }} className="truncate-3">{card.content}</Box>
             <Button className="text-danger" onClick={handleDelete}><Delete /></Button>
         </ListGroupItem>
     )
