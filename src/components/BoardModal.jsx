d94b068e904c873d7dce1428d7f175e7e87bb65e
Nana KOITA <koita.nana19@gmail.com>
2026-03-11 13:47:38 +0100

    Réparation du modal

--- src/components/BoardModal.jsx
+++ src/components/BoardModal.jsx
@@ -1,4 +1,4 @@
-import AddIcon from '@mui/icons-material/Add';
+import { Add, Edit } from '@mui/icons-material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
@@ -6,55 +6,64 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { useParams } from 'react-router';
-import { createBoard } from '../services/boardServices';
+import { createBoard, updateBoard } from '../services/boardServices';
 
-export default function BoardModal({ onResult, onCreated }) {
+export default function BoardModal({ onResult, onCreated, content = null }) {
     const { id } = useParams()
     const [open, setOpen] = useState(false);
-    const [title, setTitle] = useState("")
-    const [background, setBackground] = useState("#777777")
-    const [description, setDescription] = useState("")
+    const [title, setTitle] = useState(content ? content.title : "")
+    const [background, setBackground] = useState(content ? content.background : "#777777")
+    const [description, setDescription] = useState(content ? content.description : "")
     const token = localStorage.getItem("token")
 
     const handleOpen = () => setOpen(!open);
 
-    async function handleSubmit() {
+    const button = content ? (
+        <Button variant='outline-none' onClick={handleOpen} className="text-secondary ">
+            <Edit fontSize='large' />
+        </Button>
+    ) : (
+        <Button variant='outline-none' onClick={handleOpen} className="text-primary fs-3">
+            <Add fontSize='large' />
+        </Button>
+    )
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const boardData = {
+            title: title,
+            background: background,
+            description: description,
+            workspace: id,
+        }
+        const data = JSON.stringify({ data: boardData })
         try {
-            const boardData = JSON.stringify({
-                data: {
-                    title: title,
-                    background: background,
-                    description: description,
-                    workspace: id,
-                }
-            })
-            const res = await createBoard(token, boardData)
+            const res = !content ?
+                await createBoard(token, boardData) :
+                await updateBoard(token, content.documentId, data)
             if (!res.ok) {
                 onResult({ success: false, message: "Une erreur server s'est produite, veuillez réessayer plus tard." })
                 console.error(res)
                 return
             }
-            onCreated(id)
-            onResult({ success: true, message: "Noueau board créé!" })
+            onCreated(content ? content.documentId : id)
+            onResult({ success: true, message: "👍🏽" })
         } catch (error) {
             onResult({ success: false, message: "Une erreur réseau s'est produite, veuillez réessayer plus tard." })
             console.error(error)
         }
         handleOpen()
-        setTitle("")
-        setDescription("")
+        setTitle(content ? boardData.title : "")
+        setDescription(content ? boardData.description : "")
     }
 
     return (
         <>
-            <Button variant='warning' onClick={handleOpen} className="d-flex align-self-center justify-content-center justify-self-center">
-                <AddIcon />
-                <Typography noWrap component="div" sx={{ display: { xs: 'none', sm: 'block' } }}>
-                    Créer un board
-                </Typography>
-            </Button>
+            {button}
             <Modal show={open} onHide={handleOpen} className="rounded" centered backdrop="static">
-                <Modal.Header closeButton />
+                <Modal.Header closeButton >
+                    <Typography sx={{ fontWeight: "bolder" }}>BOARD</Typography>
+                </Modal.Header>
                 <Form className='p-2 gap-1'>
                     <Modal.Body>
                         <Box>
